0108ab8815e926522a930bc19af33297243a9cbd
maggiePoletti <maggiefp@gmail.com>
2021-10-30 13:51:14 -0400

    the most basic of basic set up. In these files, the work is done in dashboard (name subject to change)

---
+++ CMSC447/dashboard/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+#def userHome(request):
+#    titles = ["t1", "t2", "t3", "t4"]
+#    return render(request, 'home.html', {'titles': titles});
+
