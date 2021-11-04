e37377b781b87e27746eaa65fe1ef586bed142e0
maggiePoletti <maggiefp@gmail.com>
2021-11-04 11:31:48 -0400

    Added home page and upload. Upload WORKS (file is saved) but throws error...

--- CMSC447/dashboard/views.py
+++ CMSC447/dashboard/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.files.storage import FileSystemStorage
+
+from django.shortcuts import redirect
+
 # Create your views here.
 
-#def userHome(request):
-#    titles = ["t1", "t2", "t3", "t4"]
-#    return render(request, 'home.html', {'titles': titles});
+from dashboard.forms import DocumentForm
 
+def home(request):
+    titles = ["t1", "t2", "t3", "t4"]
+    return render(request, 'home.html', {'titles': titles});
+
+def upload(request):
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = DocumentForm()
+    return render(request, 'upload.html', {
+        'form': form
+    })
